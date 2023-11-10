651e5f1cec5afd42d3435252f40a71608888b6ad
LeXunAnh <leanhxuan1943@gmail.com>
2023-11-10 13:20:22 +0700

    Utest2

---
+++ Caculator Tester/UnitTest2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Caculator2;
+namespace Caculator_Tester
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private SimpleEquation s;
+
+        [TestMethod]
+        public void TestVoNghiem()
+        {
+            s = new SimpleEquation(0,1);
+            Assert.AreEqual("VN", s.SingleEquation());
+        }
+        [TestMethod]
+        public void TestCoNghiem()
+        {
+            s = new SimpleEquation(2, 1);
+            Assert.AreEqual("-0.5", s.SingleEquation());
+        }
+        [TestMethod]
+        public void TestVoSoNghiem()
+        {
+            s = new SimpleEquation(0, 0);
+            Assert.AreEqual("VSN", s.SingleEquation());
+        }
+    }
+}
---
+++ Caculator2/SimpleEquation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caculator2
+{
+    public class SimpleEquation
+    {
+        double a, b;
+        public SimpleEquation(double a,double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public string SingleEquation()
+        {
+            double e;
+            string rs;
+            if (a == 0)
+                if (b == 0)
+                    rs = "VSN";
+                else
+                    rs = "VN";
+            else
+            {
+                e = -b / a;
+                rs = e.ToString();
+            }
+                
+            return rs;
+
+        }
+    }
+}
